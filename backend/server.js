e6f2dd71660f9b4ef8e5dd7711702bd3b6ad7705
Stéfani Sant'ana <132493571+tefisss@users.noreply.github.com>
2025-03-25 16:43:09 -0300

    Initializing backend + connecting Supabase

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,17 @@
-require ('dotenv').config();
 const app = require('./src/app');
+const supabase = require('./src/config/db'); 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log('Server running on port ${PORT}'));
+
+supabase
+    .from('users')  
+    .select()
+    .then(response => {
+        if (response.error) {
+            console.log('Erro ao conectar com o Supabase: ', response.error);
+        } else {
+            console.log('Conexão com o Supabase está ok! \n TABELA USERS', response.data);
+        }
+    })
+    .catch(err => console.log('Erro ao conectar com o Supabase: ', err)); 
