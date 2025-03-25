71affe3aeb95f93f0d9425895f8e819557693273
Stéfani Sant'ana <132493571+tefisss@users.noreply.github.com>
2025-03-25 16:43:09 -0300

    Initializing backend + connecting Supabase

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,9 @@
-const {Pool} = require('pg')
-const pool = new Pool({
-    connectionString: process.env.DB_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-})
+const { createClient } = require('@supabase/supabase-js');
 
-pool.query('SELECT NOW()')
-    .then(() => console.log('Connected to supabase'))
-    .catch(err => console.log('Erro ao conectar: ', err));
+const supabaseUrl = "https://nqhypbfhiqbriyiqxmml.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xaHlwYmZoaXFicml5aXF4bW1sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEyMjAwNjIsImV4cCI6MjA1Njc5NjA2Mn0.Cybqe0UMbhMfq0W_CUyjPeEkQ0cvleUlwFLfIypBcHo"
+;
 
-module.exports = pool;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+module.exports = supabase;
