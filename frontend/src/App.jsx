ab180b2eefae948f12c9c451ba91b3d2479ba5ab
Rebeca Lara <rebecalarads@hotmail.com>
2025-03-25 20:51:01 -0300

    Frontend scope

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Login from "./components/login/Login";
+import "./components/login/Login.css"
 
 function App() {
   return <div className="App">
--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import './login.css';
 
 const Login = () => {
   return (
     <div className="container">
       <form>
-        <h1>Acesse o sistema</h1>
+        <h1>Beatz</h1>
         <div>
+          <button className="facebook">CONTINUE WITH FACEBOOK"</button>
+        </div>
+        <div>
+          <button className="apple">CONTINUE WITH APPLE"</button>
+        </div>
+        <div>
+          <button className="google">CONTINUE WITH GOOGLE</button>
+        </div>
+        <a>OR</a>
+        <br/>
+        <br/>
+        <div>
+          <a className="input-field">Email address</a>
+          <br/>
           <input type="email" placeholder="Email address" />
         </div>
         <div>
+          <a>Email address</a>
+          <br/>
           <input type="password" placeholder="Password" />
         </div>
+        <div className="forgetpassword">
+          <a href="#">Forget your password?</a>
+        </div>
         <button>LOG IN</button>
+        
+        <div className="signup">
+        <br />
+          <p>
+           Don't have an account?
+           <br/>
+           <button>Sign up for BEATZ</button>
+          </p>
+        </div>
       </form>
     </div>
   );
