315ab49bf139ef7d1a19b2d9f75b9f8c9f628a2c
Rebeca Lara <rebecalarads@hotmail.com>
2025-04-10 18:27:45 -0300

    Frontend login header changes

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -4,52 +4,55 @@ import BeatzLogo from "../../assets/BeatzLogo2.svg";
 
 const Login = () => {
   return (
-    <div className="container">
+    <div>
       <header className="beatzheader">
         <img
           src={BeatzLogo}
           alt="Beatz Logo"
           style={{ width: "150px", height: "auto" }}
         />
+        <hr />
       </header>
 
-      <form>
-        <div>
-          <button className="facebook">CONTINUE WITH FACEBOOK"</button>
-        </div>
-        <div>
-          <button className="apple">CONTINUE WITH APPLE"</button>
-        </div>
-        <div>
-          <button className="google">CONTINUE WITH GOOGLE</button>
-        </div>
-        <a>OR</a>
-        <br />
-        <br />
-        <div>
-          <a className="input-field">Email address</a>
+      <div className="container">
+        <form>
+          <div>
+            <button className="facebook">CONTINUE WITH FACEBOOK</button>
+          </div>
+          <div>
+            <button className="apple">CONTINUE WITH APPLE</button>
+          </div>
+          <div>
+            <button className="google">CONTINUE WITH GOOGLE</button>
+          </div>
+          <a>OR</a>
           <br />
-          <input type="email" placeholder="Email address" />
-        </div>
-        <div>
-          <a>Email address</a>
           <br />
-          <input type="password" placeholder="Password" />
-        </div>
-        <div className="forgetpassword">
-          <a href="#">Forget your password?</a>
-        </div>
-        <button>LOG IN</button>
-
-        <div className="signup">
-          <br />
-          <p>
-            Don't have an account?
+          <div>
+            <a className="input-field">Email address</a>
             <br />
-            <button>Sign up for BEATZ</button>
-          </p>
-        </div>
-      </form>
+            <input type="email" placeholder="Email address" />
+          </div>
+          <div>
+            <a>Password</a>
+            <br />
+            <input type="password" placeholder="Password" />
+          </div>
+          <div className="forgetpassword">
+            <a href="#">Forget your password?</a>
+          </div>
+          <button>LOG IN</button>
+
+          <div className="signup">
+            <br />
+            <p>
+              Don't have an account?
+              <br />
+              <button>Sign up for BEATZ</button>
+            </p>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
