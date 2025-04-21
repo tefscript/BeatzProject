a279b4bd04eea9d50b24b9a5a365c2d576e6d2cf
Stéfani Sant'ana <132493571+tefisss@users.noreply.github.com>
2025-04-21 19:29:12 -0300

    Rotas de Login e alterações para ESModules

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,9 +1,5 @@
-const { createClient } = require('@supabase/supabase-js');
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://nqhypbfhiqbriyiqxmml.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xaHlwYmZoaXFicml5aXF4bW1sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEyMjAwNjIsImV4cCI6MjA1Njc5NjA2Mn0.Cybqe0UMbhMfq0W_CUyjPeEkQ0cvleUlwFLfIypBcHo"
-;
+const db = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-module.exports = supabase;
+export default db;
