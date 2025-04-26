d85837e650ec983390a22aa42a8b6fa803afd2ae
Stéfani Sant'ana <132493571+tefisss@users.noreply.github.com>
2025-04-26 19:26:10 -0300

    Backend 1.2.1

--- backend/src/utils/handleError.js
+++ backend/src/utils/handleError.js
@@ -1,4 +1,4 @@
 export const handleError = (res, error) => {
     console.error(error);
-    return res.status(500).json({ error: 'Ocorreu um erro interno.' });
+    return res.status(500).json({ error: 'Algo deu errado, tente novamente mais tarde.' });
 };
